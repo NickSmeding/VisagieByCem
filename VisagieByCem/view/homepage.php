4cb91bc747488643441861a58d116f024dfda132
nick smeding <nicksmeding95@gmail.com>
2017-11-15 15:43:42 +0100

    added basic files

---
+++ VisagieByCem/view/homepage.php
@@ -0,0 +1,7 @@
+<?php
+    include_once('inc/head.php');
+?>
+<body>
+    <h1 style="text-decoration: underline;">kappersite basis</h1>
+    <h3>hierbij de mappen structuur</h3>
+</body>
---
+++ VisagieByCem/view/inc/footer.php
@@ -0,0 +1,15 @@
+<div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="copyright_text">
+                            <p class=" wow fadeInRight" data-wow-duration="1s">Copyright © 2017 - Kappersite</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
